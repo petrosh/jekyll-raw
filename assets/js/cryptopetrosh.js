e05f2f8cccc3b440ef1569e02b419c308540dc00
Alex Petrosh <petrosh@users.noreply.github.com>
2017-03-14 14:04:46 +0100

    Cross origin

--- assets/js/cryptopetrosh.js
+++ assets/js/cryptopetrosh.js
@@ -1,15 +1,24 @@
 function encrypt() {
 	var key = document.getElementById('key').value;
 	var text = document.getElementById('text').value;
-	var url = "//petrosh.altervista.org/cryptopetrosh/encrypt.php?key=";
+	var url = "https://crossorigin.me/http://petrosh.altervista.org/cryptopetrosh/encrypt.php?key=";
 	url += encodeURI(key) + "&text=" + encodeURI(text);
-	fetch(url).then(console.log);
+	fetch(url, {mode: 'no-cors',headers:{'Access-Control-Allow-Origin':'*'}}).then(function(response) {
+    return Promise.resolve(response)
+	}).then(json).then(console.log);
 }
 
 function decrypt() {
 	var key = document.getElementById('key').value;
 	var text = document.getElementById('text').value;
-	var url = "//petrosh.altervista.org/cryptopetrosh/decrypt.php?key=";
+	var url = "https://crossorigin.me/http://petrosh.altervista.org/cryptopetrosh/decrypt.php?key=";
 	url += encodeURI(key) + "&text=" + encodeURI(text);
-	fetch(url).then(console.log);
+	fetch(url, {mode: 'no-cors',headers:{'Access-Control-Allow-Origin':'*'}}).then(function(response) {
+    return Promise.resolve(response)
+  }).then(json).then(console.log);
+}
+
+function json(response) {
+	console.log(response);
+  return response.json()
 }
