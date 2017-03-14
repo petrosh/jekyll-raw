84d8aecaa5d2b1b8240cab365d2ca297118bab22
Alex Petrosh <petrosh@users.noreply.github.com>
2017-03-14 17:20:29 +0100

    Add clipboard and finalize Cryptopetrosh

--- assets/js/cryptopetrosh.js
+++ assets/js/cryptopetrosh.js
@@ -1,24 +1,47 @@
 function encrypt() {
-	var key = document.getElementById('key').value;
-	var text = document.getElementById('text').value;
-	var url = "https://crossorigin.me/http://petrosh.altervista.org/cryptopetrosh/encrypt.php?key=";
-	url += encodeURI(key) + "&text=" + encodeURI(text);
-	fetch(url, {mode: 'no-cors',headers:{'Access-Control-Allow-Origin':'*'}}).then(function(response) {
-    return Promise.resolve(response)
-	}).then(json).then(console.log);
+	var key = document.getElementById('key').value.trim();
+	var text = document.getElementById('text').value.trim();
+	var url = "http://petrosh.altervista.org/cryptopetrosh/encrypt.php?key=";
+	url += encodeURIComponent(key) + "&text=" + encodeURIComponent(text) + "&callback=read";
+	appendScript(url);
 }
 
 function decrypt() {
-	var key = document.getElementById('key').value;
-	var text = document.getElementById('text').value;
-	var url = "https://crossorigin.me/http://petrosh.altervista.org/cryptopetrosh/decrypt.php?key=";
-	url += encodeURI(key) + "&text=" + encodeURI(text);
-	fetch(url, {mode: 'no-cors',headers:{'Access-Control-Allow-Origin':'*'}}).then(function(response) {
-    return Promise.resolve(response)
-  }).then(json).then(console.log);
+	var key = document.getElementById('key').value.trim();
+	var text = document.getElementById('text').value.trim();
+	var url = "http://petrosh.altervista.org/cryptopetrosh/decrypt.php?key=";
+	url += encodeURIComponent(key) + "&text=" + encodeURIComponent(text) + "&callback=read";
+	appendScript(url);
 }
 
-function json(response) {
-	console.log(response);
-  return response.json()
+function read(s){
+	document.getElementById('text').value = s;
+	new Clipboard('button');
+	// Enable buttons
+	var all = document.getElementsByTagName("button");
+	for(var i = 0, max = all.length; i < max; i++){
+		all[i].removeAttribute("disabled");
+	}
+}
+
+function reset(){
+	document.getElementById('text').value = '';
+	document.getElementById('key').value = '';
+	document.getElementById('key').focus();
+}
+
+function appendScript(url){
+	// Disable buttons
+	var all = document.getElementsByTagName("button");
+	for(var i = 0, max = all.length; i < max; i++){
+		all[i].setAttribute('disabled', '');
+	}
+	// Append script
+	var t   = document.createElement('script');
+	t.type  = 'text/javascript';
+	t.async = true;
+	t.src = url;
+	console.log(url);
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(t, s);
 }
--- assets/pages/crypto.md
+++ assets/pages/crypto.md
@@ -4,19 +4,21 @@ permalink: crypto/
 description: Stay safe
 js:
   - polyfills/fetch.v2.0.3
+  - polyfills/clipboard.v1.6.1.min
   - cryptopetrosh
 ---
 <div id="cryptoinput">
-	<a href="http://petrosh.altervista.org/cryptopetrosh/">Original</a>
+<p><a href="http://petrosh.altervista.org/cryptopetrosh/">Original</a> is safer.</p>
 <p>
 <input type="password" id="key" placeholder="key" size="40" autofocus>
 </p>
-
 <p>
 <textarea id="text" rows="8" cols="80" placeholder="Text"></textarea>
 </p>
-
+<button onclick="reset();" type="button">RESET</button>
 <button onclick="encrypt();" type="button">ENCRYPT</button>
 <button onclick="decrypt();" type="button">DECRYPT</button>
-
+<button type="button" data-clipboard-target="#text">COPY TO CLIPBOARD</button>
 </div>
+
+{% include show-page-scripts.html %}
