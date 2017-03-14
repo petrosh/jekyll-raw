b9676740e6be15704b095f0c3ea91165621b161f
Alex Petrosh <petrosh@users.noreply.github.com>
2017-03-14 12:03:39 +0100

    check Access-Control-Allow-Origin

--- assets/js/cryptopetrosh.js
+++ assets/js/cryptopetrosh.js
@@ -3,7 +3,7 @@ function encrypt() {
 	var text = document.getElementById('text').value;
 	var url = "//petrosh.altervista.org/cryptopetrosh/encrypt.php?key=";
 	url += encodeURI(key) + "&text=" + encodeURI(text);
-	// fetch(url).then(console.log);
+	fetch(url).then(console.log);
 }
 
 function decrypt() {
@@ -11,5 +11,5 @@ function decrypt() {
 	var text = document.getElementById('text').value;
 	var url = "//petrosh.altervista.org/cryptopetrosh/decrypt.php?key=";
 	url += encodeURI(key) + "&text=" + encodeURI(text);
-	// fetch(url).then(console.log);
+	fetch(url).then(console.log);
 }
